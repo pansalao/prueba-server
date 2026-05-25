2bafbc4c42bb63d75ffb69079791bb262276b8cc
Maikol Ojeda <maikoldro19@gmail.com>
2026-05-25 13:27:22 -0400

    permisos en las seeder PDS

--- database/seeders/PlanningDataSeeder.php
+++ database/seeders/PlanningDataSeeder.php
@@ -151,17 +151,151 @@ class PlanningDataSeeder extends Seeder
 
         // --- 8. PERMISOS DEL SISTEMA ---
         $permisos = [
-            ['nombre_permiso' => 'Listar de Planificacion'],
-            ['nombre_permiso' => 'Crear de Planificacion'],
-            ['nombre_permiso' => 'Editar de Planificacion'],
-            ['nombre_permiso' => 'Ver Detalles de Planificacion'],
-            ['nombre_permiso' => 'Aprobar Planificaciones'],
-            ['nombre_permiso' => 'Configurar Calendario'],
-            ['nombre_permiso' => 'Ver Bitacora'],
-            ['nombre_permiso' => 'Gestionar Catalogos'],
+            ['id_permiso' => 1, 'nombre_permiso' => 'Index de Perfil', 'estatus' => '1'],
+            ['id_permiso' => 2, 'nombre_permiso' => 'Index de Seleccionar Rol', 'estatus' => '1'],
+            ['id_permiso' => 3, 'nombre_permiso' => 'Listar de Contenido', 'estatus' => '1'],
+            ['id_permiso' => 4, 'nombre_permiso' => 'Crear de Contenido', 'estatus' => '1'],
+            ['id_permiso' => 5, 'nombre_permiso' => 'Editar de Contenido', 'estatus' => '1'],
+            ['id_permiso' => 6, 'nombre_permiso' => 'Ver Detalles de Contenido', 'estatus' => '1'],
+            ['id_permiso' => 7, 'nombre_permiso' => 'Listar de Tema', 'estatus' => '1'],
+            ['id_permiso' => 8, 'nombre_permiso' => 'Crear de Tema', 'estatus' => '1'],
+            ['id_permiso' => 9, 'nombre_permiso' => 'Editar de Tema', 'estatus' => '1'],
+            ['id_permiso' => 10, 'nombre_permiso' => 'Ver Detalles de Tema', 'estatus' => '1'],
+            ['id_permiso' => 13, 'nombre_permiso' => 'Listar de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 14, 'nombre_permiso' => 'Crear de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 15, 'nombre_permiso' => 'Editar de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 16, 'nombre_permiso' => 'Ver Detalles de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 17, 'nombre_permiso' => 'Reporte Cumplimiento de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 18, 'nombre_permiso' => 'Reporte General de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 19, 'nombre_permiso' => 'Reporte Detallado de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 20, 'nombre_permiso' => 'Reporte de Calendario', 'estatus' => '1'],
+            ['id_permiso' => 21, 'nombre_permiso' => 'Acuerdo Aprendizaje de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 22, 'nombre_permiso' => 'Listar de Indicador Logro', 'estatus' => '1'],
+            ['id_permiso' => 23, 'nombre_permiso' => 'Crear de Indicador Logro', 'estatus' => '1'],
+            ['id_permiso' => 24, 'nombre_permiso' => 'Editar de Indicador Logro', 'estatus' => '1'],
+            ['id_permiso' => 25, 'nombre_permiso' => 'Ver Detalles de Indicador Logro', 'estatus' => '1'],
+            ['id_permiso' => 26, 'nombre_permiso' => 'Listar de Bibliografia', 'estatus' => '1'],
+            ['id_permiso' => 27, 'nombre_permiso' => 'Crear de Bibliografia', 'estatus' => '1'],
+            ['id_permiso' => 28, 'nombre_permiso' => 'Editar de Bibliografia', 'estatus' => '1'],
+            ['id_permiso' => 29, 'nombre_permiso' => 'Ver Detalles de Bibliografia', 'estatus' => '1'],
+            ['id_permiso' => 30, 'nombre_permiso' => 'Listar de Recurso', 'estatus' => '1'],
+            ['id_permiso' => 31, 'nombre_permiso' => 'Crear de Recurso', 'estatus' => '1'],
+            ['id_permiso' => 32, 'nombre_permiso' => 'Editar de Recurso', 'estatus' => '1'],
+            ['id_permiso' => 33, 'nombre_permiso' => 'Ver Detalles de Recurso', 'estatus' => '1'],
+            ['id_permiso' => 34, 'nombre_permiso' => 'Listar de Estrategia', 'estatus' => '1'],
+            ['id_permiso' => 35, 'nombre_permiso' => 'Crear de Estrategia', 'estatus' => '1'],
+            ['id_permiso' => 36, 'nombre_permiso' => 'Editar de Estrategia', 'estatus' => '1'],
+            ['id_permiso' => 37, 'nombre_permiso' => 'Ver Detalles de Estrategia', 'estatus' => '1'],
+            ['id_permiso' => 38, 'nombre_permiso' => 'Listar de Tecnica Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 39, 'nombre_permiso' => 'Crear de Tecnica Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 40, 'nombre_permiso' => 'Editar de Tecnica Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 41, 'nombre_permiso' => 'Ver Detalles de Tecnica Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 42, 'nombre_permiso' => 'Listar de Tipo Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 43, 'nombre_permiso' => 'Crear de Tipo Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 44, 'nombre_permiso' => 'Editar de Tipo Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 45, 'nombre_permiso' => 'Ver Detalles de Tipo Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 46, 'nombre_permiso' => 'Listar de Evento', 'estatus' => '1'],
+            ['id_permiso' => 47, 'nombre_permiso' => 'Crear de Evento', 'estatus' => '1'],
+            ['id_permiso' => 48, 'nombre_permiso' => 'Editar de Evento', 'estatus' => '1'],
+            ['id_permiso' => 49, 'nombre_permiso' => 'Ver Detalles de Evento', 'estatus' => '1'],
+            ['id_permiso' => 50, 'nombre_permiso' => 'Listar de Calendario', 'estatus' => '1'],
+            ['id_permiso' => 51, 'nombre_permiso' => 'Crear de Calendario', 'estatus' => '1'],
+            ['id_permiso' => 52, 'nombre_permiso' => 'Ver Detalles de Calendario', 'estatus' => '1'],
+            ['id_permiso' => 53, 'nombre_permiso' => 'Editar de Calendario', 'estatus' => '1'],
+            ['id_permiso' => 54, 'nombre_permiso' => 'Listar de Color', 'estatus' => '3'],
+            ['id_permiso' => 55, 'nombre_permiso' => 'Crear de Color', 'estatus' => '3'],
+            ['id_permiso' => 56, 'nombre_permiso' => 'Editar de Color', 'estatus' => '3'],
+            ['id_permiso' => 57, 'nombre_permiso' => 'Ver Detalles de Color', 'estatus' => '3'],
+            ['id_permiso' => 58, 'nombre_permiso' => 'Listar de Permiso', 'estatus' => '1'],
+            ['id_permiso' => 59, 'nombre_permiso' => 'Editar de Permiso', 'estatus' => '1'],
+            ['id_permiso' => 60, 'nombre_permiso' => 'Listar de Bitacora', 'estatus' => '1'],
+            ['id_permiso' => 61, 'nombre_permiso' => 'Ver Detalles de Bitacora', 'estatus' => '1'],
+            ['id_permiso' => 62, 'nombre_permiso' => 'Cambiar Estatus de Perfil', 'estatus' => '1'],
+            ['id_permiso' => 63, 'nombre_permiso' => 'Cambiar Estatus de Seleccionar Rol', 'estatus' => '1'],
+            ['id_permiso' => 64, 'nombre_permiso' => 'Cambiar Estatus de Contenido', 'estatus' => '1'],
+            ['id_permiso' => 65, 'nombre_permiso' => 'Cambiar Estatus de Tema', 'estatus' => '1'],
+            ['id_permiso' => 67, 'nombre_permiso' => 'Cambiar Estatus de Planificacion', 'estatus' => '1'],
+            ['id_permiso' => 68, 'nombre_permiso' => 'Cambiar Estatus de Calendario', 'estatus' => '1'],
+            ['id_permiso' => 69, 'nombre_permiso' => 'Cambiar Estatus de Indicador Logro', 'estatus' => '1'],
+            ['id_permiso' => 70, 'nombre_permiso' => 'Cambiar Estatus de Bibliografia', 'estatus' => '1'],
+            ['id_permiso' => 71, 'nombre_permiso' => 'Cambiar Estatus de Recurso', 'estatus' => '1'],
+            ['id_permiso' => 72, 'nombre_permiso' => 'Cambiar Estatus de Estrategia', 'estatus' => '1'],
+            ['id_permiso' => 73, 'nombre_permiso' => 'Cambiar Estatus de Tecnica Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 74, 'nombre_permiso' => 'Cambiar Estatus de Tipo Evaluacion', 'estatus' => '1'],
+            ['id_permiso' => 75, 'nombre_permiso' => 'Cambiar Estatus de Evento', 'estatus' => '1'],
+            ['id_permiso' => 76, 'nombre_permiso' => 'Cambiar Estatus de Color', 'estatus' => '3'],
+            ['id_permiso' => 77, 'nombre_permiso' => 'Cambiar Estatus de Permiso', 'estatus' => '1'],
+            ['id_permiso' => 78, 'nombre_permiso' => 'Cambiar Estatus de Bitacora', 'estatus' => '1'],
+            ['id_permiso' => 79, 'nombre_permiso' => 'Listar de Firma', 'estatus' => '3'],
+            ['id_permiso' => 80, 'nombre_permiso' => 'Crear de Firma', 'estatus' => '3'],
+            ['id_permiso' => 81, 'nombre_permiso' => 'Editar de Firma', 'estatus' => '3'],
+            ['id_permiso' => 82, 'nombre_permiso' => 'Ver Detalles de Firma', 'estatus' => '3'],
+            ['id_permiso' => 83, 'nombre_permiso' => 'Cambiar Estatus de Firma', 'estatus' => '1'],
+            ['id_permiso' => 84, 'nombre_permiso' => 'Mi Firma de Firma', 'estatus' => '1'],
         ];
+
         foreach ($permisos as $p) {
-            DB::table('permiso')->updateOrInsert(['nombre_permiso' => $p['nombre_permiso']], $p);
+            DB::table('permiso')->updateOrInsert(['id_permiso' => $p['id_permiso']], $p);
         }
+
+        // --- 9. ASOCIACIONES ROL-PERMISO ---
+        // Vicerrector (Roles 31 y 4)
+        $vicerrectorRoles = [31, 4];
+        $vicerrectorPerms = [
+            1, 2,  // Perfil, Seleccionar Rol
+            46, 47, 48, 49, 75,  // Eventos: Listar, Crear, Editar, Ver Detalles, Cambiar Estatus
+            50, 51, 52, 53, 68, 20,  // Calendario: Listar, Crear, Ver Detalles, Editar, Cambiar Estatus, Reporte de Calendario
+            58, 59, 77,  // Permisos: Listar, Editar, Cambiar Estatus
+        ];
+
+        // Coordinador (Roles 1, 5, 11, 30)
+        $coordinadorRoles = [1, 5, 11, 30];
+        $coordinadorPerms = [
+            1, 2,  // Perfil, Seleccionar Rol
+            7, 8, 9, 10, 65,  // Temas: Listar, Crear, Editar, Ver Detalles, Cambiar Estatus
+            3, 4, 5, 6, 64,  // Contenidos: Listar, Crear, Editar, Ver Detalles, Cambiar Estatus
+            13, 16, 17, 18, 19, 67,  // Planificacion: Listar, Ver Detalles, Reportes (Cumplimiento, General, Detallado), Cambiar Estatus
+        ];
+
+        // Profesor (Rol 2)
+        $profesorRoles = [2];
+        $profesorPerms = [
+            1, 2,  // Perfil, Seleccionar Rol
+            13, 14, 15, 16, 17, 18, 19, 21, 67,  // Planificacion: Listar, Crear, Editar, Ver Detalles, Reportes, Acuerdo de Aprendizaje, Estatus
+            34, 35, 36, 37, 72,  // Estrategia: Listar, Crear, Editar, Ver Detalles, Cambiar Estatus
+            30, 31, 32, 33, 71,  // Recursos: Listar, Crear, Editar, Ver Detalles, Cambiar Estatus
+            38, 39, 40, 41, 73,  // Técnica Evaluación: Listar, Crear, Editar, Ver Detalles, Cambiar Estatus
+            42, 43, 44, 45, 74,  // Tipo Evaluación: Listar, Crear, Editar, Ver Detalles, Cambiar Estatus
+        ];
+
+        // Estudiante (Rol 3)
+        $estudianteRoles = [3];
+        $estudiantePerms = [
+            1, 2,  // Perfil, Seleccionar Rol
+            13, 16,  // Planificacion: Listar, Ver Detalles
+            46, 49,  // Evento: Listar, Ver Detalles
+            50, 52,  // Calendario: Listar, Ver Detalles
+        ];
+
+        // Limpiar todas las relaciones existentes en rol_permiso
+        DB::table('rol_permiso')->delete();
+
+        // Función auxiliar para registrar los mapeos
+        $insertMapping = function ($roles, $perms) {
+            foreach ($roles as $roleId) {
+                foreach ($perms as $permId) {
+                    DB::table('rol_permiso')->insert([
+                        'id_rol' => $roleId,
+                        'id_permiso' => $permId,
+                        'estatus' => '1',
+                    ]);
+                }
+            }
+        };
+
+        $insertMapping($vicerrectorRoles, $vicerrectorPerms);
+        $insertMapping($coordinadorRoles, $coordinadorPerms);
+        $insertMapping($profesorRoles, $profesorPerms);
+        $insertMapping($estudianteRoles, $estudiantePerms);
     }
 }
